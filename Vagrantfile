e41227215380cd9f15557505b1e6ab8ee5cab282
Chris Collins <chrisjamescollins@gmail.com>
2015-12-15 13:03:04 +0000

    Added ability to install Ansible from source.
    Install Ansible from source, handle copying of SSH keys.

--- Vagrantfile
+++ Vagrantfile
@@ -11,12 +11,16 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   ip = "192.168.111.111"
   copy_ansible_hosts = false
   ansible_project_executable = false
+  install_ansible_from_source = true
+  private_key_path = nil
+  public_key_path = nil
 
   # End of configuration.
 
   box_name = use_64_bit ? "precise64" : "precise32"
   config.vm.box = box_name
   config.vm.box_url = "http://files.vagrantup.com/" + box_name + ".box"
+  ssh_path = "/home/vagrant/.ssh"
 
   config.vm.define :ansible_controller do |ansible_controller|
     ansible_controller.vm.network :private_network, ip: ip
@@ -26,6 +30,31 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
     ansible_controller.vm.synced_folder "ansible-project", "/home/vagrant/ansible-project", :mount_options => ansible_project_executable ? nil : ["dmode=777,fmode=666"]
 
-    ansible_controller.vm.provision :shell, :path => "build.sh", :args => copy_ansible_hosts ? "-c" : nil
+    if !private_key_path.nil?
+      private_key_destination = ssh_path + "/id_rsa"
+
+      ansible_controller.vm.provision :file, source: private_key_path, destination: private_key_destination
+      ansible_controller.vm.provision :shell, inline: "chmod 600 " + private_key_destination
+    end
+
+    if !public_key_path.nil?
+      public_key_destination = ssh_path + "/id_rsa.pub"
+
+      ansible_controller.vm.provision :file, source: public_key_path, destination: public_key_destination
+      ansible_controller.vm.provision :shell, inline: "chmod 600 " + public_key_destination
+      ansible_controller.vm.provision :shell, inline: "cat " + public_key_destination + " >> " + ssh_path + "/authorized_keys"
+    end
+
+    build_args = ""
+
+    if copy_ansible_hosts
+      build_args += "--copy-hosts "
+    end
+
+    if install_ansible_from_source
+      build_args += "--ansible-from-source "
+    end
+
+    ansible_controller.vm.provision :shell, :path => "build.sh", :args => build_args
   end
 end
--- build.sh
+++ build.sh
@@ -1,36 +1,83 @@
 #!/bin/bash
 #
 # Arguments:
-#    -c Copy the hosts file in the ansible-project directory to the Ansible default hosts file.
-#
+#    -c|--copy-hosts Copy the hosts file in the ansible-project directory to the Ansible default hosts file.
+#    -s|--ansible-from-source Install Ansible from source, rather than apt-get.
 
+# Constants.
 ANSIBLE_PROJECT_HOSTS=/home/vagrant/ansible-project/hosts
 ANSIBLE_HOSTS=/etc/ansible/hosts
+ANSIBLE_GIT_REPO_URL=git://github.com/ansible/ansible.git
+ANSIBLE_SOURCE_INSTALL_PATH=/opt/ansible
+SSH_DIR=/home/vagrant/.ssh
+SSH_PUBLIC_KEY_PATH=$SSH_DIR/id_rsa.pub
 
-echo "Installing Ansible..."
+# Install Ansible from source.
+function installAnsibleFromSource() {
+    echo "Installing Ansible from source..."
 
+    pip install paramiko PyYAML Jinja2 httplib2 six markupsafe
+
+    git clone $ANSIBLE_GIT_REPO_URL --recursive $ANSIBLE_SOURCE_INSTALL_PATH
+    cd $ANSIBLE_SOURCE_INSTALL_PATH
+    source ./hacking/env-setup
+}
+
+# Install Ansible from apt-get.
+function installAnsibleFromAptGet() {
+    echo "Installing Ansible from apt-get..."
+
+    apt-get install -y python-paramiko python-yaml python-jinja2 python-simplejson
+    pip install ansible
+}
+
+# Copy the ansible projects file to etc/ansible/hosts for convenience.
+function copyAnsibleProjectHosts() {
+    if [[ -f $ANSIBLE_PROJECT_HOSTS ]]; then
+        echo "Copying ansible-project hosts file to $ANSIBLE_HOSTS..."
+
+        cp $ANSIBLE_PROJECT_HOSTS $ANSIBLE_HOSTS
+        chown vagrant:vagrant $ANSIBLE_HOSTS
+        chmod 622 $ANSIBLE_HOSTS
+    else
+        echo "$ANSIBLE_PROJECT_HOSTS was not found, could not copy to $ANSIBLE_HOSTS."
+    fi
+}
+
+# Main.
+copy_ansible_project_hosts=0
+
+echo "Installing apt dependencies..."
 apt-get update -qq
-apt-get install python-pip python-paramiko python-yaml python-jinja2 python-simplejson -y
-pip install ansible
+apt-get install -y git python-pip
 
-while getopts ":c" opt; do
-    case $opt in
-        c)
-            if [[ -f $ANSIBLE_PROJECT_HOSTS ]]; then
-                echo "Copying ansible-project hosts file to $ANSIBLE_HOSTS..."
-
-                cp $ANSIBLE_PROJECT_HOSTS $ANSIBLE_HOSTS
-                chown vagrant:vagrant $ANSIBLE_HOSTS
-                chmod 622 $ANSIBLE_HOSTS
-            else
-                echo "$ANSIBLE_PROJECT_HOSTS was not found, could not copy to $ANSIBLE_HOSTS."
-            fi
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            exit 1
-            ;;
+while :; do
+    case $1 in
+        -c|--copy-hosts)
+            copyAnsibleProjectHosts
+        ;;
+        -s|--ansible-from-source)
+            install_ansible_from_source=1
+        ;;
+        --)
+            shift
+            break
+        ;;
+        -?*)
+            echo "Invalid option: -$1"
+        ;;
+        *)
+            break
+        ;;
     esac
+
+    shift
 done
+
+if [[ install_ansible_from_source -eq 1 ]]; then
+    installAnsibleFromSource
+else
+    installAnsibleFromAptGet
+fi
 
 echo "Finished."
